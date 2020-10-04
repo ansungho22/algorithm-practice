a8827802c2925de0c408e92b916a574fd9414934
An Sung Ho <ansungho22@gmail.com>
2020-10-05 00:01:58 +0900

    10.5 00:00

---
+++ algorithm/No.10/소스.cpp
@@ -0,0 +1,26 @@
+//baekjoon_1541
+
+#include <iostream>
+
+#include <vector>
+
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s;
+	vector<int> a;
+	int n = 0;
+	cin >> s;
+	a.push_back(stoi(s));
+	for (int i = 0; i < s.size(); i++)
+		if (s[i] == '+')
+			a.back() += stoi(s.substr(i + 1, 6));
+		else if (s[i] == '-') {
+			n -= a.back();
+			a.push_back(stoi(s.substr(i + 1, 6)));
+		}
+	cout << n + (a[0] * 2) - a.back() << endl;
+	return 0;
+}
