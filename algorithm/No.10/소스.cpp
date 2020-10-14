ce9dcee3e3cdcba6b5471473c858f7f6d2dd5fe0
An Sung Ho <ansungho22@gmail.com>
2020-10-14 22:17:19 +0900

    gtg

--- algorithm/No.10/소스.cpp
+++ algorithm/No.10/소스.cpp
@@ -1,9 +1,6 @@
 //baekjoon_1541
-
 #include <iostream>
-
 #include <vector>
-
 #include <string>
 using namespace std;
 
@@ -13,14 +10,19 @@ int main() {
 	int n = 0;
 	cin >> s;
 	a.push_back(stoi(s));
-	a.back() = 4;
 	for (int i = 0; i < s.size(); i++)
+	{
 		if (s[i] == '+')
 			a.back() += stoi(s.substr(i + 1, 6));
 		else if (s[i] == '-') {
-			n -= a.back();
 			a.push_back(stoi(s.substr(i + 1, 6)));
 		}
-	cout << n + (a[0] * 2) - a.back() << endl;
+	}
+	for (int i = 0; i < a.size(); i++)
+	{
+		n -= a[i];
+	}
+
+	cout << n + (a[0] * 2)  << endl;
 	return 0;
 }
