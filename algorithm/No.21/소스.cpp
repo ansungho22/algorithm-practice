8019558a515fd45759d99f608994bcb48bc6b44c
An Sung Ho <ansungho22@gmail.com>
2020-11-26 01:24:17 +0900

    정렬문제

--- algorithm/No.21/소스.cpp
+++ algorithm/No.21/소스.cpp
@@ -8,40 +8,35 @@
 using namespace std;
 
 int main() {
-	int N,temp,Sum=0,mx=-1,mode;
+	int N, temp, mode, max , min ;
+	int Sum = 0, mx = -1;
 	int arr[8001] = { 0, };
 	cin >> N;
-	vector<int> v,mode_v;
+	vector<int> v, mode_v;
 	for (int i = 0; i < N; i++)
 	{
 		cin >> temp;
 		v.push_back(temp);
 		Sum += temp;
-		temp = (temp <= 0) ? abs(temp) : temp + 4000;
+		temp = (temp <= 0) ? 4000 + temp : temp + 4000;
 		arr[temp]++;
 		if (arr[temp] > mx) {
 			mx = arr[temp];
 			mode = temp;
 		}
 	}
-	for (int i = 4001; i <= 8001; i++) {
+	sort(v.begin(), v.end());
+	max = v.back()+4000;
+	min = v.front()+4000;
+	for (int i = min; i <= max; i++) {
 		if (arr[i] == mx) {
 			mode = i;
-			mode = (mode <= 4000) ? -mode : mode - 4000;
+			mode = mode - 4000;
 			mode_v.push_back(mode);
 		}
 	}
-	for (int i = 4000; i >=0; i--) {
-		if (arr[i] == mx) {
-			mode = i;
-			mode = (mode <= 4000) ? -mode : mode - 4000;
-			mode_v.push_back(mode);
-		}
-	}
-
 	mode = (mode_v.size() >= 2) ? mode_v[1] : mode_v[0];
-	sort(v.begin(),v.end());
-	cout << (int)floor((Sum / N)+0.5) << endl;
+	cout << round(float(Sum) / float(N)) << endl;
 	cout << v[N / 2] << endl;
 	cout << mode << endl;
 	cout << v.back() - v.front() << endl;
