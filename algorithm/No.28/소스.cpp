feee5763327edac2759ff78ee720e32fa250a165
An Sung Ho <ansungho22@gmail.com>
2020-12-04 01:31:59 +0900

    최단거리 구하기

--- algorithm/No.28/소스.cpp
+++ algorithm/No.28/소스.cpp
@@ -1,41 +1,56 @@
+//구름 최단거리 구하기
 #include<iostream>
 #include<vector>
 #include <algorithm>
+#include<queue>
+#define MAX 100
 using namespace std;
-vector<int> v;
-void dfs(int (*borad)[10], bool (*T_Fborad)[10], int x, int y,int n) {
-	if (borad[x][y] == 0 || x>=n || y>=n || x < 0 || y < 0 )return;
-	if (x == n - 1 && y == n - 1)v.push_back(borad[x][y]);
-	int vx[] = { 1,-1,0,0 };
-	int vy[] = { 0,0,1,-1 };
-	T_Fborad[x][y] = true;
-	for (int i = 0; i < n; i++)
-	{
-		if (borad[x + vx[i]][y + vy[i]] != 0 && T_Fborad[x + vx[i]][y + vy[i]]==false) {
-			borad[x + vx[i]][y + vy[i]] = borad[x][y] + 1;
-			dfs(borad, T_Fborad, x + vx[i], y + vy[i], n);
+int N;
+int arr[MAX][MAX] = { 0, };
+bool visit[MAX][MAX] = { false };
+int vx[4] = { 1,-1,0,0 };
+int vy[4] = { 0,0,1,-1 };
+
+int bfs(int x, int y,int cnt) {
+	queue <pair<pair<int, int>, int>> que;
+	que.push(make_pair(make_pair(x, y), cnt));
+
+	while (!que.empty()) {
+		x = que.front().first.first;
+		y = que.front().first.second;
+		cnt = que.front().second;
+		que.pop();
+
+		if (x == N - 1 && y == N - 1)return cnt;
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = x + vx[i];
+			int ny = y + vy[i];
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N)continue;
+			if (arr[nx][ny] == 1 && !visit[nx][ny]) {
+				visit[nx][ny] = true;
+				que.push(make_pair(make_pair(nx, ny), cnt + 1));
+			}
 		}
 	}
+	
+	
 	
 
 }
 
 int main() {
-	int N;
-	int input;
-	int borad[10][10] = { 0 };
-	bool T_Fborad[10][10]{ false };
 	cin >> N;
 	for (int i = 0; i< N; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			cin >> borad[i][j];
+			cin >> arr[i][j];
 		
 		}
 	}
-	dfs(borad, T_Fborad, 0,0, N);
-	sort(v.begin(), v.end());
-	cout << v.front();
+	visit[0][0] = true;
+	cout << bfs(0, 0, 1);
+	return 0;
 	
 }
