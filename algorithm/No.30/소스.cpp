81640499b1487ed35b2aba926c39b3d26defe80a
An Sung Ho <ansungho22@gmail.com>
2020-12-05 00:57:24 +0900

    토마토

--- algorithm/No.30/소스.cpp
+++ algorithm/No.30/소스.cpp
@@ -67,3 +67,48 @@ int main() {
 		return 0;
 	}
 }
+
+//백준7576번 토마토 파이썬
+//import sys
+//from collections import deque
+//read = sys.stdin.readline
+//X, Y = map(int, read().split())
+//box = []
+//loc = deque()
+//for i in range(Y) :
+//	row = list(map(int, read().split()))
+//	for j in range(X) :
+//		if row[j] == 1 :
+//			loc.append([i, j])
+//			box.append(row)
+//
+//			dx = [-1, 0, 1, 0]
+//			dy = [0, 1, 0, -1]
+//			cnt = 1
+//			while loc:
+//y, x = loc.popleft()
+//
+//for l in range(4) :
+//	move_x = x + dx[l]
+//	move_y = y + dy[l]
+//
+//	if move_x < 0 or move_y < 0 or move_x >= X or move_y >= Y or box[move_y][move_x] == -1 :
+//		continue
+//
+//		if box[move_y][move_x] == 0 :
+//			box[move_y][move_x] = box[y][x] + 1
+//			loc.append([move_y, move_x])
+//			cnt = box[move_y][move_x]
+//
+//			fuck = True
+//			for k in box :
+//if 0 in k :
+//print(-1)
+//fuck = False
+//break
+//
+//if fuck == True :
+//	if cnt == 1 :
+//		print(0)
+//		elif cnt != 1 :
+//		print(cnt - 1)
