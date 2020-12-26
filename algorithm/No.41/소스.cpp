1a06168d6aa182167624f70351acdb34f276566a
An Sung Ho <ansungho22@gmail.com>
2020-12-26 17:12:41 +0900

    등굣길

--- algorithm/No.41/소스.cpp
+++ algorithm/No.41/소스.cpp
@@ -3,23 +3,23 @@
 #include<iostream>
 using namespace std;
 int solution(int m, int n, vector<vector<int>> puddles) {
+	int puddles_map[101][101] = { 0, };
 	int map[101][101] = { 0, };
-	int d[101][101];
 
-	// set puddles into map.
+	// set puddles into puddles_map.
 	for (auto v : puddles)
-		map[v[1]][v[0]] = -1;
+		puddles_map[v[1]][v[0]] = -1;
 
-	d[1][0] = 1;
+	map[1][0] = 1;
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= m; j++) {
-			if (map[i][j] == -1)
-				d[i][j] = 0;
+			if (puddles_map[i][j] == -1)
+				map[i][j] = 0;
 			else
-				d[i][j] = (d[i - 1][j] + d[i][j - 1]) % 1000000007;
+				map[i][j] = (map[i - 1][j] + map[i][j - 1]) % 1000000007;
 		}
 	}
-	return d[n][m];
+	return map[n][m];
 }
 int main() {
 	int m = 4, n = 3;
