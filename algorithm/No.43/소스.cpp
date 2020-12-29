961f04bfc40d5593a3217e93ec8df3c0cd6d4c2f
An Sung Ho <ansungho22@gmail.com>
2020-12-29 18:17:02 +0900

    주식가격(수정)

--- algorithm/No.43/소스.cpp
+++ algorithm/No.43/소스.cpp
@@ -1,29 +1,19 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <iostream>
+#include <vector>
 using namespace std;
-// 테스트통과 효율성 x
-int* solution(int prices[], size_t prices_len) {
-	int* answer = (int*)malloc(sizeof(int) * prices_len);
-	bool check[100000] = { false, };
-	for (int i = 0; i < prices_len; i++)
-	{
-		answer[i] = 0;
-		check[i] = true;
-		for (int j = 0; j < i; j++)
-		{
-			if (check[j] == true ) {
-				answer[j]++;
-			}
-			if (prices[j] > prices[i]) check[j] = false;
-		}
-	}
-	return answer;
-}
-
-int main() {
-	int prices[] = {1, 2, 3, 2, 3};
-	size_t prices_len = sizeof(prices) / sizeof(prices[0]);
-	int* ans = solution(prices, prices_len);
+vector<int> solution(vector<int> prices) {
+    int n = prices.size();
+    vector<int> answer;
+    for (int i = 0; i < n - 2; i++) {
+        int ans = 1;
+        for (int j = i + 1; j < n - 1; j++) {
+            if (prices[i] > prices[j]) {
+                break;
+            }
+            ans++;
+        }
+        answer.push_back(ans);
+    }
+    answer.push_back(1);
+    answer.push_back(0);
+    return answer;
 }
