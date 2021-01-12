6349ef57f5b582035f510a01d5fd660b870d39bc
An Sung Ho <ansungho22@gmail.com>
2021-01-12 22:05:09 +0900

    큰 수 만들기

--- algorithm/No.62/소스.cpp
+++ algorithm/No.62/소스.cpp
@@ -30,3 +30,26 @@ int main() {
     int k = 2;
     cout << solution(number, k);
 }
+
+//string solution(string number, int k) {
+//    string answer = "";
+//    answer = number.substr(k);
+//    for (int i = k - 1; i >= 0; i--) {
+//        int j = 0;
+//        do {
+//            if (number[i] >= answer[j]) {
+//                char temp = answer[j];
+//                answer[j] = number[i];
+//                number[i] = temp;
+//                j++;
+//            }
+//            else {
+//                break;
+//            }
+//        } while (1);
+//    }
+//
+//
+//
+//    return answer;
+//}
