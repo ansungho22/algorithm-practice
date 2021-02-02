bb72f97854827dec8ee3c43371fce466b1bb5d03
An Sung Ho <ansungho22@gmail.com>
2021-02-02 23:27:10 +0900

    징검다리수정

--- algorithm/No.76/소스.cpp
+++ algorithm/No.76/소스.cpp
@@ -4,35 +4,34 @@
 #include <iostream>
 
 using namespace std;
-int solution(int distance, vector<int> rocks, int n) {
-    int answer = 0;
-    sort(rocks.begin(), rocks.end());
-    int l = 0;
-    int r = distance;
-    while (l <= r) {
-        int mid = (l + r) / 2;
-        int removeIdx = -1;
-        int removeCount = 0;
-        for (int i = 0; i <= rocks.size(); i++) {
-            int d;
-            if (i == 0) {
-                d = rocks[i];
-            }
-            else {
-                if (i == rocks.size()) d = distance - rocks[rocks.size() - 1];
-                else d = rocks[i] - rocks[removeIdx];
-            }
-            if (d < mid) removeCount++;
-            else removeIdx = i;
-        }
-        if (removeCount > n) {
-            r = mid - 1;
-        }
-        else if (removeCount <= n) {
-            l = mid + 1;
-            answer = max(answer, mid);
-        }
-    }
 
-    return answer;
+int solution(int distance, vector<int> rocks, int n) {
+
+    sort(rocks.begin(),rocks.end());
+    long left = 1;
+    long right = distance;
+    long answer = 0;
+
+    while (left <= right) {
+        int removeRockCnt = 0;
+        int prev = 0;
+        long minDistance = (left + right) / 2;
+        for (int rock : rocks) {
+            if ((rock - prev) < minDistance) removeRockCnt++;
+            else prev = rock;
+        }
+        if (distance - prev < minDistance) removeRockCnt++;
+        if (removeRockCnt <= n) {
+            answer = max(answer, minDistance);
+            left = minDistance + 1;
+        }
+        else right = minDistance - 1;
+    }
+    return (int)answer;
+}
+int main() {
+    int distance = 25;
+    vector<int> rocks = { 2,14,11,21,17 };
+    int n = 2;
+    cout << solution(distance, rocks, n);
 }
