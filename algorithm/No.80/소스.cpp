96430d013300c0f79c287099ac6ea486a77dbdc2
ansungho <ansungho22@gmail.com>
2021-06-15 20:20:25 +0900

    프로그래머스 문제
    소수만들기

--- algorithm/No.80/소스.cpp
+++ algorithm/No.80/소스.cpp
@@ -1,8 +1,28 @@
-#include <iostream>
-#include <stdio.h>
+//프로그래머스 소수만들기
+#include <vector>
 using namespace std;
 
-int main() {
-	int a = 3 + 5;
-	cout << " 하하하하";
+bool isPrime(int num) {
+    if (num == 0 || num == 1) return false;
+    for (int i = 2; i < num / 2; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            for (int k = j + 1; k < nums.size(); k++) {
+                int n = nums[i] + nums[j] + nums[k];
+                if (isPrime(n) == true) {
+                    answer++;
+                }
+            }
+        }
+    }
+
+    return answer;
 }
