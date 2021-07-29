96d2d0ff3f66c277169889740cadd844ccd280aa
ansungho <ansungho22@gmail.com>
2021-07-29 16:14:23 +0900

    프로그래머스 문자열 압축

---
+++ algorithm/No.90/소스.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include<iostream>
+#include<algorithm>
+
+
+using namespace std;
+
+int solution(string s) {
+    int answer = s.size();
+
+    for (int i = 1; i <= s.size() / 2; i++) {
+        string convert, temp;
+
+        int cnt = 1;
+        temp = s.substr(0, i);
+
+        for (int j = i; j < s.size(); j += i) {
+            if (temp == s.substr(j, i)) cnt++;
+            else {
+                if (cnt > 1) convert += to_string(cnt);
+                convert += temp;
+                temp = s.substr(j, i);
+                cnt = 1;
+            }
+        }
+
+        if (cnt > 1) convert += to_string(cnt);
+
+        convert += temp;
+        answer = max(answer, int(convert.size()));
+    }
+    return answer;
+}
