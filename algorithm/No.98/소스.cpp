beef4514fb536a93d8ea732f99516fe42f0d025a
ansungho <ansungho22@gmail.com>
2021-09-01 18:18:28 +0900

    카카오프렌즈 컬러링북

--- algorithm/No.98/소스.cpp
+++ algorithm/No.98/소스.cpp
@@ -1,27 +1,33 @@
 #include <vector>
 #include <map>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 int int_board[101][101] = { 0, };
-int bool_board[101][101] = { false, };
+bool bool_board[101][101] = { false, };
 
 int dx[4] = { 0,0,-1,1 };
 int dy[4] = { 1,-1,0,0 };
 
-map<int, int> m;
-void dfs(int col, int row, int m, int n) {
+map<int, int> mapset;
+
+void dfs(int col, int row, int m, int n, int cell) {
+	int_board[col][row] = 0;
+	bool_board[col][row] = false;
+	if (mapset.find(cell) != mapset.end()) mapset[cell]++;
+	else {
+		mapset[cell] = 1;
+	}
 	for (int i = 0; i < 4; i++)
 	{
 		int my = col + dy[i];
 		int mx = row + dx[i];
-		if ((0 <= mx && mx < n) && (0 <= my && my < m) && int_board[col][row] == int_board[my][mx]) {
-			if (m[int_board[col][row]]);
-		}
+		if ((0 <= mx && mx < n) && (0 <= my && my < m) && cell == int_board[my][mx] && bool_board[my][mx] == true)dfs(my, mx, m, n, cell);
+
 	}
 }
 vector<int> solution(int m, int n, vector<vector<int>> picture) {
-	int number_of_area = 0;
-	int max_size_of_one_area = 0;
-
+	vector<pair<int, int>> v;
 	for (int col = 0; col < m; col++)
 	{
 		for (int row = 0; row < n; row++)
@@ -36,13 +42,17 @@ vector<int> solution(int m, int n, vector<vector<int>> picture) {
 		for (int row = 0; row < n; row++)
 		{
 			if (int_board[col][row] == 0)continue;
-			dfs(col, row, m, n);
+			int key = int_board[col][row];
+			dfs(col, row, m, n, int_board[col][row]);
+			v.push_back(make_pair(key, mapset[key]));
+			mapset[key] = 0;
 		}
 	}
-
-
+	sort(v.begin(),v.end(), [](pair<int, int> a, pair<int, int> b) {
+		return a.second > b.second;
+		});
 	vector<int> answer(2);
-	answer[0] = number_of_area;
-	answer[1] = max_size_of_one_area;
+	answer[0] = v.size();
+	answer[1] = v.front().second;
 	return answer;
 }
